4973e279c25892ebdbfa307ca4d269e8fa901164
Roberto Maciel <lmaciel1303@gmail.com>
2020-09-01 18:24:56 -0500

    Modify home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,33 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://scontent-qro1-1.cdninstagram.com/v/t51.2885-19/s150x150/97566921_2973768799380412_5562195854791540736_n.jpg?_nc_ht=scontent-qro1-1.cdninstagram.com&_nc_ohc=2t8QuRbIh-0AX_Pdw94&oh=3021f27c64355e3677a4e3a36b8ee56b&oe=5F764467" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div>
+                <h1>freecodecamp</h1>
             </div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>252</strong> follows</div>
+            </div>
+            <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
+            <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
+            <div><a href="#">www.freecodecamp.org</a></div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://scontent-qro1-1.cdninstagram.com/v/t51.2885-15/e35/c0.115.933.933a/s150x150/118408019_445411183083690_3655664231758381001_n.jpg?_nc_ht=scontent-qro1-1.cdninstagram.com&_nc_cat=106&_nc_ohc=W8HEpKz3x3cAX8YYUHd&oh=d9e39fdf52f011fce333bc6bda636d3e&oe=5F78081B" style="width: 100%">
+        </div>
+        <div class="col-4">
+            <img src="https://scontent-qro1-1.cdninstagram.com/v/t51.2885-15/e35/c0.112.929.929a/s150x150/118605679_1451236268600542_2050874606066190756_n.jpg?_nc_ht=scontent-qro1-1.cdninstagram.com&_nc_cat=110&_nc_ohc=hUzCn5rOvSUAX_gy03D&oh=7b0b1aad3931cc69cce591e6d5da2d94&oe=5F7779DD" style="width: 100%">
+        </div>
+        <div class="col-4">
+            <img src="https://scontent-qro1-1.cdninstagram.com/v/t51.2885-15/e35/c0.112.929.929a/s150x150/118605679_1451236268600542_2050874606066190756_n.jpg?_nc_ht=scontent-qro1-1.cdninstagram.com&_nc_cat=110&_nc_ohc=hUzCn5rOvSUAX_gy03D&oh=7b0b1aad3931cc69cce591e6d5da2d94&oe=5F7779DD" style="width: 100%">
         </div>
     </div>
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,8 +23,10 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand d-flex align-items-center justify-content-center" href="{{ url('/') }}">
+                    {{-- Laravel begins to search for our files in public, so we don't have to put it --}}
+                    <img src="/svg/free-code-camp-logo.svg" style="height: 25px; border-right: 1px solid black;" class="pr-3 align-self-center">
+                    <div class="pl-3 align-self-center">freeCodeGram</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
