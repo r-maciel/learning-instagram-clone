e147db04976adcaa9d6f83d813baa08c649d5963
Roberto Maciel <lmaciel1303@gmail.com>
2020-09-05 16:04:06 -0500

    Update posts.shiw view

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
-            <img src="https://scontent-qro1-1.cdninstagram.com/v/t51.2885-19/s150x150/97566921_2973768799380412_5562195854791540736_n.jpg?_nc_ht=scontent-qro1-1.cdninstagram.com&_nc_ohc=2t8QuRbIh-0AX_Pdw94&oh=3021f27c64355e3677a4e3a36b8ee56b&oe=5F764467" class="rounded-circle">
+            <img src="/storage/{{ $user->profile->image }}" class="rounded-circle w-100">
         </div>
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
